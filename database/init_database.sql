965cdc70e0af00934dd0d17be14279e0418981fd
Alex <dalekov.alex@gmail.com>
2024-11-24 03:06:06 +0200

    add pigeons, comment friendships

--- database/init_database.sql
+++ database/init_database.sql
@@ -7,19 +7,12 @@ CREATE TABLE users (
 );
 
 -- Create the 'friendships' table to store user friendships
--- NOT TESTED
-CREATE TABLE friendships (
-    id SERIAL PRIMARY KEY,                          -- Unique ID for each friendship
-    user_id INT NOT NULL,                           -- First user's ID
-    friend_id INT NOT NULL,                         -- Second user's ID
-    status VARCHAR(20) DEFAULT 'pending',           -- Friendship status: 'pending', 'accepted', 'declined'
-    requested_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Timestamp of the friend request
-    accepted_at TIMESTAMP,                          -- Timestamp when the friendship is accepted
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-    CONSTRAINT fk_friend FOREIGN KEY (friend_id) REFERENCES users (id) ON DELETE CASCADE,
-    CONSTRAINT unique_friendship UNIQUE (user_id, friend_id), -- Prevent duplicate friendships
-    CONSTRAINT check_no_reverse_friends CHECK (user_id < friend_id); -- Ensure user_id < friend_id for consistency 
-);
+-- CREATE TABLE friendships (
+--     id SERIAL PRIMARY KEY,                          -- Unique ID for each friendship
+--     user_id INT NOT NULL,                           -- First user's ID
+--     friend_id INT NOT NULL,                         -- Second user's ID
+--     status VARCHAR(20) DEFAULT 'pending',           -- Friendship status: 'pending', 'accepted', 'declined'
+-- );
 
 
 -- Create the 'mails' table to track sent messages
@@ -30,9 +23,17 @@ CREATE TABLE mails (
     content TEXT NOT NULL,                         -- Message content
     status VARCHAR(50) NOT NULL DEFAULT 'in_transit', -- Message status (in_transit, delivered, lost)
     sent_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP, -- Time the mail was sent
-    delivery_time TIMESTAMP,                        -- Time the mail was delivered or lost
-    delivery_eta TIMESTAMP                         -- Estimated time of arrival for the mail
+    delivery_eta TIMESTAMP,                         -- Estimated time of arrival for the mail
+    delivery_time TIMESTAMP                        -- Time the mail was delivered or lost
 );
+
+CREATE TABLE pigeons (
+    id SERIAL PRIMARY KEY,
+    sender_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE, -- Owner's user ID
+    recipient_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE, -- Recipient's user ID
+    mail_id INT NOT NULL REFERENCES mails(id) ON DELETE CASCADE, -- Mail ID
+    status VARCHAR(20) DEFAULT 'flying',         -- Pigeon status: 'flying', 'dead'
+
 
 CREATE TABLE user_locations (
     id SERIAL PRIMARY KEY,
